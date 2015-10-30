9c67a12a5f0ade5cc3cf770a1ac6bb3ec99e6923
Zach Baker <zach.r.baker1@gmail.com>
2015-10-30 15:16:23 -0600

    ish is working

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -2,12 +2,21 @@ angular.module('practiceApp').controller('mainCtrl', ['$scope', 'myService', fun
 
 	$scope.getData = function () {
 		myService.getData().then(function (dataFromService) {
-			$scope.pokeIsh = dataFromService;
+			$scope.moves = dataFromService;
 			console.log('thisisPokeish', $scope.pokeIsh)
 		})
 	}
-	$scope.test = "test";
-	$scope.getData();
+	
+	
+	$scope.showHide = true;
+	
+	$scope.moveDataFunc = function(url){
+		$scope.showHide = false;
+		myService.getMoveDetails(url).then(function(ish){
+			$scope.moveDetails = ish;
+		})
+		
+	}
 
 }]);
 
--- js/myService.js
+++ js/myService.js
@@ -1,18 +1,31 @@
 angular.module('practiceApp').service('myService', ['$http', '$q', function ($http, $q) {
 
+
+var baseURL = "http://pokeapi.co";
+
 	this.getData = function () {
-		alert("getData is firing");
-		alert(typeof $q);
+		
 		var deferred = $q.defer();
 		$http({
 			method: 'GET',
-			url: "http://pokeapi.co/api/v1/pokemon/1/"
+			url: baseURL + "/api/v1/pokemon/1/"
 		}).then(function (response) {
-			var parsedResponse = response.data;
+			var parsedResponse = response.data.moves;
 			console.log('this is the parsedResponse', parsedResponse);
 			deferred.resolve(parsedResponse);
 		})
 		return deferred.promise;
 	};
+	
+	this.getMoveDetails = function(url){
+			var deferred = $q.defer();	
+			 $http({
+			method: 'GET',
+			url: baseURL + url
+		}).then(function(response){
+			deferred.resolve(response.data);
+		});
+		return deferred.promise;
+	}
 
 }]);
